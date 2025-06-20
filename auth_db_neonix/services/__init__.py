750c351d159048da97d98bd2e77f8433b0150008
Nico88-Vs <112125692+Nico88-Vs@users.noreply.github.com>
2025-06-20 20:59:09 +0200

    versions updated

--- auth_db_neonix/services/__init__.py
+++ auth_db_neonix/services/__init__.py
@@ -1,6 +1,4 @@
 from .data_retriver_service import DataRetriever
-from auth_db_neonix.api.fire_client_service import FirebaseClient
 from .sqlite_service import SQLiteManager
 
-__all__ = ["SQLiteManager", "DataRetriever",
-           "FirebaseClient"]
+__all__ = ["SQLiteManager", "DataRetriever"]
